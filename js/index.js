6df3cd6df66068a68df818d424a8a34a2685e164
naxisinc <33284023+naxisinc@users.noreply.github.com>
2018-10-21 21:18:05 -0400

    Working in mega color

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let objWinner = [];
 let count = 0;
+let mega_color;
 
 $(document).ready(() => {
   $('#form').on('submit', function(e) {
@@ -9,54 +10,53 @@ $(document).ready(() => {
     let value = parseInt($('#number').val());
     if (count <= 5) {
       if (search(value)) {
-        $('#num' + count).text(value);
-        coloring();
-        for (let i = 1; i <= objWinner.length; i++) {
-          switch (objWinner[i - 1].color) {
-            case 0:
-              $('#num' + i).addClass('zero');
-              break;
-            case 1:
-              $('#num' + i).addClass('one');
-              break;
-            case 2:
-              $('#num' + i).addClass('two');
-              break;
-            case 3:
-              $('#num' + i).addClass('three');
-              break;
-            case 4:
-              $('#num' + i).addClass('four');
-          }
+        $('#num' + (count - 1)).text(value);
+        setColor();
+        for (let i = 0; i < objWinner.length; i++) {
+          coloring(objWinner[i].color, i);
         }
       } else {
-        $('#num' + count).addClass('black');
+        $('#num' + (count - 1)).addClass('black');
       }
     } else if (count === 6) {
-      if (isMegaBall(value)) {
-        $('#num6').addClass('six');
-        $('#num6').text(value);
+      let res = isMegaBall(value);
+      if (res !== -1) {
+        $('#num5').text(value);
+        if (res === 1) {
+          coloring(objWinner[0].color, 5);
+        } else {
+          coloring(parseInt(mega_color), 5);
+        }
       } else {
-        $('#num6').addClass('black');
+        $('#num5').addClass('black');
       }
     }
     $('#number').val('');
   });
 });
 
-function search(value) {
-  let found = false;
-  for (const numbers of arr) {
-    for (const number of numbers) {
-      if (number === value) {
-        found = true;
-        break;
-      }
-    }
+function coloring(color, id) {
+  if (color === 0) {
+    $('#num' + id).addClass('zero');
+  } else if (color === 1) {
+    $('#num' + id).addClass('one');
+  } else if (color === 2) {
+    $('#num' + id).addClass('two');
+  } else if (color === 3) {
+    $('#num' + id).addClass('three');
+  } else if (color === 4) {
+    $('#num' + id).addClass('four');
+  } else if (color === 5) {
+    $('#num' + id).addClass('five');
   }
-  if (found) {
-    objWinner.push({ value });
-    return true;
+}
+
+function search(value) {
+  for (const numbers of arr) {
+    if (_.includes(numbers, value)) {
+      objWinner.push({ value });
+      return true;
+    }
   }
   return false;
 }
@@ -79,7 +79,7 @@ function isIncluded() {
   return false;
 }
 
-function coloring() {
+function setColor() {
   if (objWinner.length === 1) {
     objWinner[0].color = 0;
   } else {
@@ -96,6 +96,8 @@ function coloring() {
         }
       }
 
+      console.log(maxInt);
+
       if (_.includes(maxInt, objWinner[currentPosition].value)) {
         for (let i = 0; i <= currentPosition; i++) {
           if (_.includes(maxInt, objWinner[i].value)) {
@@ -109,13 +111,36 @@ function coloring() {
       objWinner[currentPosition].color = 0;
     }
   }
+  console.log(objWinner);
 }
 
 function isMegaBall(value) {
-  for (i = 0; i < mega.length; i++) {
-    if (mega[i] === value) {
-      return true;
+  if (_.includes(mega, value)) {
+    let arr_aux = [];
+    let pos = 0;
+    for (let i = 0; i < mega.length; i++) {
+      if (mega[i] === value) {
+        arr_aux[pos++] = i;
+      }
     }
+
+    let arr_winner = ObjectToArray();
+    for (let i = 0; i < arr_aux.length; i++) {
+      if (_.difference(arr[arr_aux[i]], arr_winner).length === 0) {
+        return 1; // Bingo!!!
+      }
+    }
+    let colors = [];
+    for (let i = 0; i < objWinner.length; i++) {
+      colors[i] = objWinner[i].color;
+    }
+    mega_color = _.head(
+      _(colors)
+        .countBy()
+        .entries()
+        .maxBy('[1]')
+    );
+    return 0; // Esta pero no fue Bingo!
   }
-  return false;
+  return -1; // No esta
 }
